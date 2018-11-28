4f2f2abfd0cb667478c8d461e53075e9da648c2d
Aleksandr Chutkov <assus0@yandex.ru>
2018-11-28 11:14:56 +0300

    Optimized code

--- newConsole/newConsole.js
+++ newConsole/newConsole.js
@@ -1,24 +1,11 @@
 'use strict'
 
-let log = console.log;
-
-console.log = function() {
-    log.apply(this, Array.prototype.slice.call(arguments));
-    localStorage.setItem("Last log", Array.prototype.slice.call(arguments));
-}
-
-let error = console.error;
-
-console.error = function() {
-    error.apply(this, Array.prototype.slice.call(arguments));
-    localStorage.setItem("Last error", Array.prototype.slice.call(arguments));
-}
-
-let warn = console.warn;
-
-console.warn = function() {
-    warn.apply(this, Array.prototype.slice.call(arguments));
-    localStorage.setItem("Last warn", Array.prototype.slice.call(arguments));
+for (let key in console) {
+    let con = console[key];
+    console[key] = function() {
+        con.apply(this, Array.prototype.slice.call(arguments));
+        localStorage.setItem("Message", Array.prototype.slice.call(arguments));
+    }
 }
 
 window.onload = function() {
